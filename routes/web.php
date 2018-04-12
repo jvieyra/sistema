e671bcc59cd82409e2bede2a3ecf55b9717b9b41
jvieyra <vieyrapez@gmail.com>
2018-04-12 18:45:45 -0500

    controller and routes articulos

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('contenido/contenido');
 });
 
+
+//:::::::::CATEGORIAS ::::::::::::::::::::::::::::
 //obtener datos verbo get
 Route::get('/categoria','CategoriaController@index');
 
@@ -27,3 +29,19 @@ Route::put('/categoria/actualizar','CategoriaController@update');
 //status
 Route::put('/categoria/desactivar','CategoriaController@desactivar');
 Route::put('/categoria/activar','CategoriaController@activar');
+
+
+
+//:::::::::ARTICULOS ::::::::::::::::::::::::::::
+//obtener datos verbo get
+Route::get('/articulo','ArticuloController@index');
+
+//store articuloController
+Route::post('/articulo/registrar','ArticuloController@store');
+
+//Update verbo put
+Route::put('/articulo/actualizar','ArticuloController@update');
+
+//status
+Route::put('/articulo/desactivar','ArticuloController@desactivar');
+Route::put('/articulo/activar','ArticuloController@activar');
